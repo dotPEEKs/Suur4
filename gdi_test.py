0b6e08589d79f7a06011f2ec771a7c1c3d8fb342
Alperen <148077179+dotPEEKs@users.noreply.github.com>
2024-01-14 19:20:42 +0300

    Delete gdi_test.py

--- gdi_test.py
+++
@@ -1,9 +0,0 @@
-
-import random
-from Suura.includes.payloads.gdi_payloads import Screen,VAR
-from Suura.includes.util import gdi32
-from Suura.includes.util import GetDC
-dc = GetDC()
-while True:
-    gdi32.BitBlt(dc,random.randint(1,10),random.randint(1,20),Screen.SCREEN_X,Screen.SCREEN_X,dc,0,0,VAR.SRCCOPY)
-    gdi32.BitBlt(dc,random.randint(1,10) % 10 - 20,random.randint(1,20),Screen.SCREEN_X,Screen.SCREEN_X,dc,0,0,VAR.SRCCOPY)
